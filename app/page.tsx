ffb9cd4d34826651a5bce7cc151dd54feb92bbb4
Mirzin <mirzinsheroqueabdullah@gmail.com>
2025-05-05 17:01:50 +0530

    Fixed responsive issue

--- app/page.tsx
+++ app/page.tsx
@@ -19,19 +19,17 @@ export default function Home() {
         alt="logo"
       />
       <div className="h-screen flex flex-col items-center justify-center">
-        <h1
-          className={`text-white text-5xl sm:text-8xl md:text-8xl lg:text-8xl xl:text-8xl m-5 ${majorMono.className}`}
-        >
-          ZYROQUE
-        </h1>
-        <h2 className="text-white text-2xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-5xl">
-          Coming soon..
-        </h2>
-        <Link
-          className="absolute bottom-10 "
-          href="https://www.youtube.com/@zyroque"
-          target="_blank"
-        >
+        <div className="mb-60">
+          <h1
+            className={`text-white text-5xl sm:text-8xl md:text-8xl lg:text-8xl xl:text-8xl m-5 ${majorMono.className}`}
+          >
+            ZYROQUE
+          </h1>
+          <h2 className="text-white text-center text-2xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-5xl">
+            Coming soon..
+          </h2>
+        </div>
+        <Link href="https://www.youtube.com/@zyroque" target="_blank">
           <Image
             className="scale-60 sm:scale-100 md:scale-100 lg:scale-100 xl-scale-100 transition-transform duration-300 ease-in-out transform hover:scale-120 hover:cursor-pointer"
             src="/images/yt_logo_rgb_dark.png"
